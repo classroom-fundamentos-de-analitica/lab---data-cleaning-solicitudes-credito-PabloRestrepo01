4f81386eca7f02e1ba8de8c96032cb5364ee7c22
PabloRestrepo01 <70071301+PabloRestrepo01@users.noreply.github.com>
2022-11-03 17:25:23 -0500

    Codigo

--- pregunta.py
+++ pregunta.py
@@ -17,4 +17,18 @@ def clean_data():
     # Inserte su código aquí
     #
 
+    df.dropna(axis=0, inplace=True)
+    df.monto_del_credito = df.monto_del_credito.str.strip("$")
+    df.monto_del_credito = df.monto_del_credito.str.replace(",","")
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+    df.monto_del_credito = df.monto_del_credito.astype(int)
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
+
+    for fila in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
+        df[fila] = df[fila].str.lower()
+        df[fila] = df[fila].str.replace('_', ' ')
+        df[fila] = df[fila].str.replace('-', ' ')
+
+    df.drop_duplicates(inplace=True)
+
     return df
